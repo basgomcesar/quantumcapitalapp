6ed3ee6059520e4e97be12531ce625ca42ee2300
basgomcesar <cesarbasiliogomez@gmail.com>
2025-06-08 23:55:32 -0600

    fix responsive login

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,19 +1,19 @@
 export function HeroSection() {
-    return (
-      <div className="min-h-screen container pl-40 pt-30 flex flex-col justify-center py-10">
-        <h1 className="text-5xl font-semibold text-white mb-10 text-left">
-          Quantum <br />
-          Capital
-        </h1>
-        <p className="text-2xl italic text-white mb-10 text-left">
-          En Quantum Capital creemos que el acceso a tu historial crediticio
-          debe ser claro, seguro y accesible...
-        </p>
-        <img
-          src="/printer-image.png"
-          alt="Printer"
-          className="w-3/4 max-w-md"
-        />
-      </div>
-    );
-  }
+  return (
+    <div className="hidden sm:flex min-h-screen container pl-40 pt-30 flex-col justify-center py-10 sm:pl-20 sm:pt-20 lg:pl-40 lg:pt-30">
+      <h1 className="text-5xl font-semibold text-white mb-10 text-left">
+        Quantum <br />
+        Capital
+      </h1>
+      <p className="text-2xl italic text-white mb-10 text-left">
+        En Quantum Capital creemos que el acceso a tu historial crediticio
+        debe ser claro, seguro y accesible...
+      </p>
+      <img
+        src="/printer-image.png"
+        alt="Printer"
+        className="w-3/4 max-w-md"
+      />
+    </div>
+  );
+}
