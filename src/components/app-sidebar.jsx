be2ee1f5745231c7115e354b451a27a58de2f9d6
basgomcesar <cesarbasiliogomez@gmail.com>
2025-06-13 18:57:49 -0600

    fix sidebar

--- src/components/app-sidebar.jsx
+++ src/components/app-sidebar.jsx
@@ -6,6 +6,7 @@ import {
   BookOpen,
   Bot,
   Command,
+  Landmark,
   Frame,
   LifeBuoy,
   Map,
@@ -71,7 +72,7 @@ export function AppSidebar({ ...props }) {
             <SidebarMenuButton size="lg" asChild>
               <a href="#">
                 <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                  <Command className="size-4" />
+                  <Landmark className="size-4" />
                 </div>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-medium">Quantum Capital</span>
